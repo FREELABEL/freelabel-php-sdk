dc00ddbc92c2b939cb8f21b4ee50d8d00ca5c61e
AteeqRGondal <ateeq@cabmyride.com>
2021-12-15 21:55:13 +0500

    Update models

--- src/Freelabel/Resource/Product/Product.php
+++ src/Freelabel/Resource/Product/Product.php
@@ -2,6 +2,7 @@
 
 namespace Freelabel\Resource\Product;
 use Freelabel\Http\HttpClient;
+use Freelabel\Model\Base\BaseModelList;
 use Freelabel\Resource\Base\BaseResource;
 
 /**
@@ -20,4 +21,57 @@ class Product extends BaseResource
         $this->setResourceUrl($this->resourceUrl);
         parent::__construct($httpClient);
     }
+
+    public function getColors($id)
+    {
+        $resourceUrl = $this->resourceUrl . (($id) ? '/' . $id : null) . '/colors';
+        list($status, , $body) = $this->httpClient->sendHttpRequest(\Freelabel\Http\HttpClient::REQUEST_GET, $resourceUrl);
+
+        if ($status === 200) {
+            $body = json_decode($body);
+            $items = $body->data;
+            unset($body->data);
+
+            $baseList = new BaseModelList();
+            $baseList->loadFromArray($body);
+
+            foreach ($items as $item) {
+                /** @psalm-suppress UndefinedClass */
+                $model = new \Freelabel\Model\Product\ProductColor($this->httpClient);
+
+                $message           = $model->loadFromArray($item);
+                $baseList->items[] = $message;
+            }
+            return $baseList;
+        }
+
+        return $this->processRequest($status, $body);
+    }
+    public function getVariants($id)
+    {
+        $resourceUrl = $this->resourceUrl . (($id) ? '/' . $id : null) . '/product-variants';
+        list($status, , $body) = $this->httpClient->sendHttpRequest(\Freelabel\Http\HttpClient::REQUEST_GET, $resourceUrl);
+
+        if ($status === 200) {
+            $body = json_decode($body);
+            $items = $body->data;
+            unset($body->data);
+
+            $baseList = new BaseModelList();
+            $baseList->loadFromArray($body);
+
+            foreach ($items as $item) {
+                /** @psalm-suppress UndefinedClass */
+                $model = new \Freelabel\Model\Product\ProductVariant($this->httpClient);
+
+                $message           = $model->loadFromArray($item);
+                $baseList->items[] = $message;
+            }
+            return $baseList;
+        }
+
+        return $this->processRequest($status, $body);
+    }
+
+
 }
