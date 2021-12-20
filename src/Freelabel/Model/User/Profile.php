dcfa1c9af96ed696770703cca8f0e2d2c6a24402
AteeqRGondal <ateeq@cabmyride.com>
2021-12-20 18:44:01 +0500

    Profile

---
+++ src/Freelabel/Model/User/Profile.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Freelabel\Model\Product;
+
+
+use Freelabel\Model\Base\BaseModel;
+
+/**
+ * Class Product
+ *
+ * @package Freelabel\Models
+ */
+class Profile extends BaseModel
+{
+
+    public $id;
+    public $reference_no;
+    public $product_category_id;
+    public $product_subcategory_id;
+    public $profile_id;
+    public $image;
+    public $thumbnail;
+    public $retail_price;
+    public $attributes;
+    public $tags;
+    public $description;
+    public $product_type;
+    public $is_active;
+    public $type;
+    public $title;
+    public $subtitle;
+    public $price;
+    public $variants;
+    public $images;
+    public $category;
+    public $subcategory;
+    public $colors;
+    public $created_at;
+    public $updated_at;
+}
---
+++ src/Freelabel/Resource/User/Profile.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Freelabel\Resource\Product;
+use Freelabel\Http\HttpClient;
+use Freelabel\Model\Base\BaseModelList;
+use Freelabel\Resource\Base\BaseResource;
+
+/**
+ * Class Profile
+ *
+ * @package Freelabel\Resources
+ */
+class Profile extends BaseResource
+{
+    protected $resourceUrl = 'user/profile';
+
+    public function __construct(HttpClient $httpClient)
+    {
+        $this->setModel(new \Freelabel\Model\Product\Product());
+        $this->setResourceUrl($this->resourceUrl);
+        parent::__construct($httpClient);
+    }
+
+    public function getCollections($id)
+    {
+        $resourceUrl = $this->resourceUrl . '/' . $id . '/collections';
+        list($status, , $body) = $this->httpClient->sendHttpRequest(\Freelabel\Http\HttpClient::REQUEST_GET, $resourceUrl);
+
+        if ($status === 200) {
+            $body = json_decode($body);
+            $items = $body->data;
+            unset($body->data);
+
+            $baseList = new BaseModelList();
+            $baseList->loadFromArray($body);
+
+            foreach ($items as $item) {
+                /** @psalm-suppress UndefinedClass */
+                $model = new \Freelabel\Model\Product\Collection($this->httpClient);
+
+                $message           = $model->loadFromArray($item);
+                $baseList->items[] = $message;
+            }
+            return $baseList;
+        }
+
+        return $this->processRequest($status, $body);
+    }
+    public function getProducts($id)
+    {
+        $resourceUrl = $this->resourceUrl . '/' . $id . '/products';
+        list($status, , $body) = $this->httpClient->sendHttpRequest(\Freelabel\Http\HttpClient::REQUEST_GET, $resourceUrl);
+
+        if ($status === 200) {
+            $body = json_decode($body);
+            $items = $body->data;
+            unset($body->data);
+
+            $baseList = new BaseModelList();
+            $baseList->loadFromArray($body);
+
+            foreach ($items as $item) {
+                /** @psalm-suppress UndefinedClass */
+                $model = new \Freelabel\Model\Product\ProductVariant($this->httpClient);
+
+                $message           = $model->loadFromArray($item);
+                $baseList->items[] = $message;
+            }
+            return $baseList;
+        }
+
+        return $this->processRequest($status, $body);
+    }
+
+
+}
